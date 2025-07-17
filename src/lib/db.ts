c2ee7e5c15e0214e79a4adfc539558d145e431f8
Vishnu Nambiar <vishnunambiar2006@gmail.com>
2025-07-17 16:16:32 +0000

    Backhodi.live123

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,6 +2,9 @@
 import { drizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { Client } from 'pg';
 import * as schema from './schema';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: './src/.env' });
 
 const client = new Client({
   connectionString: process.env.POSTGRES_URL,
