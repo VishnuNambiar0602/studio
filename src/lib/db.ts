f6c02a18e41025c159d578958d556ed0105202c5
Vishnu Nambiar <vishnunambiar2006@gmail.com>
2025-10-27 17:18:44 +0000

    whats the issue

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -15,6 +15,9 @@ if (!connectionString) {
   throw new Error('POSTGRES_URL is not set in the environment variables. Please ensure it is defined in your .env file.');
 }
 
+// Append sslmode=require for Supabase connection.
+const connectionUrl = `${connectionString}?sslmode=require`;
+
 // Disable prefetch as it is not supported for "Transaction" pool mode
-const client = postgres(connectionString, { prepare: false });
+const client = postgres(connectionUrl, { prepare: false });
 export const db = drizzle(client, { schema });
